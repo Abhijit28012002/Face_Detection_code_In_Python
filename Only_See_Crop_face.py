8506074ca62d75d9b6e3509b6d64163936cb29d5
Abhijit Majumder <91789931+Abhijit28012002@users.noreply.github.com>
2023-06-27 21:48:13 +0530

    Update Only_See_Crop_face.py

--- Only_See_Crop_face.py
+++ Only_See_Crop_face.py
@@ -2,15 +2,16 @@ import cv2
 cap=cv2.VideoCapture(0)
 while True:
     status,photo=cap.read()
+    #photo=cv2.cvtColor(photo,cv2.COLOR_BGR2HLS)
     model=cv2.CascadeClassifier("raw.githubuhaarcascade_frontalface_default.xml")
     myface=model.detectMultiScale(photo)
     x1=myface[0][0]
     y1=myface[0][1]
     x2=myface[0][0]+myface[0][2]
     y2=myface[0][1]+myface[0][3]
-    cv2.rectangle(photo,(x1,y1),(x2,y2),[255,0,0],5)
-    myphoto=photo[x1:x2,y1:y2]
-    cv2.imshow("my photo",myphoto)
+    #cv2.rectangle(photo,(x1,y1),(x2,y2),[255,0,0],5)
+    photo=photo[y1:y2,x1:x2]
+    cv2.imshow("my photo",photo)
     if cv2.waitKey(100)==13:
         break
     
